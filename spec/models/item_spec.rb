d145b69575ea3fe0e2002aa675fdfe7e18c60234
miyaharatakuya <miya0703t16@gmail.com>
2020-12-24 10:40:36 +0900

    テストコード

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,5 +1,73 @@
 require 'rails_helper'
 
 RSpec.describe Item, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @item = FactoryBot.build(:item)
+  end
+
+  describe "商品出品" do
+    it "name,text,condition_id,price,category_id,postage_id,sipping_area_id,sipping_date_idが存在すれば出品できる" do
+      expect(@item).to be_valid
+    end
+    it "nameが空だと出品できない" do
+      @item.name = ''
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Name can't be blank")
+    end
+    it "textが空だと出品できない" do
+      @item.text = ''
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Text can't be blank")
+    end
+    it "priceが空だと出品できない" do
+      @item.price = ''
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Price is not a number")
+    end
+    it "priceが299以下だと出品できない" do
+      @item.price = 299
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Price must be greater than or equal to 300")
+    end
+    it "priceが10000000以上だと出品できない" do
+      @item.price = 10000000
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Price must be less than or equal to 9999999")
+    end
+    it "priceが全角数字だと出品できない" do
+      @item.price ="３００"
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Price is not a number")
+    end
+    it "condition_idが空だと出品できない" do
+      @item.condition_id = 1
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Condition must be other than 1")
+    end
+    it "category_idが空だと出品できない" do
+      @item.category_id = 1
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Category must be other than 1")
+    end
+    it "postage_idが空だと出品できない" do
+      @item.postage_id = 1
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Postage must be other than 1")
+    end
+    it "sipping_area_idが空だと出品できない" do
+      @item.sipping_area_id = 1
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Sipping area must be other than 1")
+    end
+    it "sipping_date_idが空だと出品できない" do
+      @item.sipping_date_id = 1
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Sipping date must be other than 1")
+    end
+    it "imageが空だと出品できない" do
+      @item.image = nil
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Image can't be blank")
+    end
+  end
 end
